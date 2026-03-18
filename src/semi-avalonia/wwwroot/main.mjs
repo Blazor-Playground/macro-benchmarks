016973423b6873ffcd701baf3bda3d18f2d07d64
pavelsavara <pavel.savara@gmail.com>
2026-03-18 12:57:35 +0100

    avalonia works

--- src/semi-avalonia/wwwroot/main.mjs
+++ src/semi-avalonia/wwwroot/main.mjs
@@ -1,13 +1,40 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 import { dotnet } from './_framework/dotnet.js'
 
-const is_browser = typeof window != "undefined";
-if (!is_browser) throw new Error(`Expected to be running in a browser`);
+function setManagedReady() {
+    globalThis.dotnet_managed_ready = performance.now();
+    globalThis.bench_results = {
+        'time-to-create-dotnet': Math.round(globalThis.dotnet_created - globalThis.js_loaded),
+        'time-to-reach-managed': Math.round(globalThis.dotnet_managed_ready - globalThis.js_loaded),
+        'wasm-memory-size': globalThis.getDotnetRuntime(0).Module.HEAPU8.byteLength,
+    };
+    globalThis.bench_complete = true;
+    const el = globalThis.document?.getElementById('status');
+    if (el) {
+        el.textContent = JSON.stringify(globalThis.bench_results, null, 2);
+    }
+}
 
-const dotnetRuntime = await dotnet
-    .withDiagnosticTracing(false)
-    .withApplicationArgumentsFromQuery()
-    .create();
+async function outer() {
+    globalThis.js_loaded = performance.now();
 
-const config = dotnetRuntime.getConfig();
+    const { setModuleImports, runMain } = await dotnet
+        .withApplicationArgumentsFromQuery()
+        .create();
 
-await dotnetRuntime.runMain(config.mainAssemblyName, [globalThis.location.href]);
+    setModuleImports('main.mjs', {
+        bench: {
+            setManagedReady,
+        }
+    });
+
+    globalThis.dotnet_created = performance.now();
+    globalThis.bench_results = {};
+
+    await runMain("Semi.Avalonia.Demo", []);
+
+}
+
+await outer();
